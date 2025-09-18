88aaa493c38f6508f71579791d6e127d00086361
aamirjamil25999 <aamirjamil25999@gmail.com>
2025-09-18 14:15:38 +0530

    admin done

---
+++ admin/dist/@/assets/AnimatedContent-DY5wzDdG.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-BYm5_yiY.js";const a=({children:t})=>e.jsx("div",{className:"tab tab-enter",children:t});export{a as A};
---
+++ admin/dist/@/assets/useDisableForDemo-B8A4vcYj.js
@@ -0,0 +1,1 @@
+import"./index-BYm5_yiY.js";import"./toast-9lrki7ye.js";const s=()=>({handleDisableForDemo:()=>!1});export{s as u};
